29f04231a98c93e8f9eac04ec8423352728eb651
coder38611 <54740236+coder38611@users.noreply.github.com>
2021-03-12 17:07:59 +0900

    Minor Link Fix

--- scrap_weather.py
+++ scrap_weather.py
@@ -7,7 +7,7 @@ def prepare_soup():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36'}
     r = requests.get(
-        'https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%EC%9A%B8%EC%82%B0+%EC%8B%A0%EC%A0%952%EB%8F%99+%EB%82%A0%EC%94%A8&oquery=%EC%8B%A0%EC%A0%952%EB%8F%99+%EB%82%A0%EC%94%A8&tqi=hZ7h6wp0J14ssCMgp1KssssstC8-332622', headers=headers)
+        'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%EB%82%A0%EC%94%A8', headers=headers)
     bs = BeautifulSoup(r.text, 'lxml')
     return bs
 
